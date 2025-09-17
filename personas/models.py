85566c442a9ad03fc6ff5eedeb7ff71a77fa598a
JoelR <jrodrigu106@cuc.edu.co>
2025-09-17 14:49:28 -0500

    se crea el modelo tasks

--- personas/models.py
+++ personas/models.py
@@ -6,6 +6,8 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=20, null= False, blank=False)
     apellido = models.CharField(max_length=20, null= False, blank=False)
     correo = models.CharField(max_length=20, null= False, blank=False, unique=False)
+    password = models.CharField(max_length=10, null=False, blank=False)
+    rol = models.IntegerField(max_length=1, null=True, blank=True)
     
     class Meta:
         db_table="personas"
---
+++ tasks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Tasks(models.Model):
+    # codigo = models.CharField(max_length=10, null=False)
+    titulo = models.CharField(max_length=20, null=False)
+    descripcion = models.CharField(max_length=100, null=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_inicio = models.DateTimeField(null=False)
+    fecha_fin = models.DateTimeField(null=False)
+    estado = models.IntegerField(null=False)
+    
+    class Meta:
+        db_table = 'tasks'
